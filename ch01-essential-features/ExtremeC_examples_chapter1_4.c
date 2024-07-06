118c93a612803d2431845b9e0dd0b40b3b6e5df0
Huiseong Lim <105583102+limhuiseong@users.noreply.github.com>
2024-07-06 13:10:09 +0000

    Docs: Add comments to explain code

--- ch01-essential-features/ExtremeC_examples_chapter1_4.c
+++ ch01-essential-features/ExtremeC_examples_chapter1_4.c
@@ -7,6 +7,8 @@
 #define CMD(NAME) \
   char NAME ## _cmd[256]  = ""; \
   strcpy(NAME ## _cmd, #NAME);
+// # 연산자는 매개변수를 한 쌍의 따옴표로 감싼 문자 형태로 반환한다.
+// ## 연산자는 다른 요소와 결합해 변수명을 만든다.
 
 int main(int argc, char** argv) {
 
